2ab086fb1651cc982d5f2336a18ecf8c17ea5332
Piyush Rathore <piyushrathore0007@gmail.com>
2019-08-30 18:46:40 +0530

    init

--- app/models/shop.rb
+++
@@ -1,8 +0,0 @@
-class Shop < ApplicationRecord
-	def self.get_shop
-		s=Shop.first
-		ShopifyAPI::Base.site = "https://#{s.api_key}:#{s.password}@#{s.name}.myshopify.com"
-		ShopifyAPI::Base.api_version = s.api_version
-		ShopifyAPI::Shop.current
-	end
-end
